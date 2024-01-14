964d5b17e1c8152da8f308de96bae252b0d3a6ac
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-01-14 12:34:56 +0530

    updated

---
+++ Bugs.js
@@ -0,0 +1,18 @@
+function printUserAccess(user){
+    if(user.isAdmin){
+        let accessLevel = "Admin";
+    } else{
+        let accessLevel = "User";
+    }
+    console.log(accessLevel);
+}
+
+const user1 = {
+    name: "Archis",
+    isAdmin: true
+};
+
+printUserAccess(user1);
+//BUG: scope 
+//Error: ReferenceError
+//Solution: either console in block scopes seperately or use var keyword
