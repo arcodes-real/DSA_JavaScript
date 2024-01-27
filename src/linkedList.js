aa7487a752b7438167a8a86c97559d19ade8fc05
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-01-27 14:00:51 +0530

    LinkedList_remove

--- src/linkedList.js
+++ src/linkedList.js
@@ -73,6 +73,27 @@ class LinkedList{
         this.size++;
     }
 
+    removeFrom(index){
+        if(index < 0 || index > this.size){
+            return null;
+        }
+        let removedNode;
+        if(index === 0){
+            removedNode = this.head;
+            this.head = this.head.next;
+        }
+        else{
+            let prev = this.head;
+            for(let i=0; i<index-1; i++){
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next;
+        }
+        this.size--;
+        return removedNode.value;
+    }
+
     print(){
         if(this.isEmpty()){
             console.log("List is Empty");
@@ -103,7 +124,7 @@ list.insert(60,2);
 list.insert(70,2);
 
 
-
+list.removeFrom(2);
 
 list.print();
 console.log(list.isEmpty());
