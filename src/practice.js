312c723c0c7bc058fd845d512bdc38c2f225696b
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-04-05 21:48:11 +0530

    shallow copying concept

--- src/practice.js
+++ src/practice.js
@@ -1,16 +1,24 @@
-function findMax(arr){
-    if(arr.length === 0){
-        return undefined
+// function subArray(arr){
+//     let subarrays = [];
+//     for(let i = 0; i<arr.length; i++){
+//         for(let j = i+1; j<= arr.length; j++){
+//             let subarray = arr.slice(i,j)
+//             subarrays.push(subarray)
+//         }
+//     }
+//     return subarrays
+// }
+// console.log(subArray([1,-2,3,4,5,-2,-1,3]))
+const people = [
+    {
+        name:"Archis",
+        age: 24
+    },
+    {
+        name:"Deshna",
+        age: 22
     }
-    let max = arr[0]
-    for(let i = 1; i < arr.length; i++){
-        if(arr[i] > max){
-            max = arr[i]
-        }
-    }
-    return max
-}
-console.log(findMax([1,2,-4,3]))
-
-
-
+    ]
+const employee = [...people];
+// employee[0].name = "Archis Datta";
+console.log(employee[1] === people[1]);
