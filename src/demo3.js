eeb5c6ccb1ec81194f9583be3b7da5f7485dde25
Archisman Datta <mpd19i017@iiitdm.ac.in>
2024-04-05 14:12:47 +0530

    maximum subarray sum

--- src/demo3.js
+++ src/demo3.js
@@ -19,5 +19,38 @@ function findMax(arr){
 
 console.log(findMax([1,2,4,3,7,5,8,90,1000]))
 
+// extracting subarrays
+function subArray(arr){
+    let subarrays = [];
+    for(let s = 0; s<arr.length-1; s++){
+        for(let l =s+1; l<=arr.length; l++){
+             let subarray = arr.slice(s,l)
+             subarrays.push(subarray)
+        }
+    }
+    return subarrays;
+
+    }
+console.log(subArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+// Maximum subarray sum : Kadane's Algorithm
+function maxSubarraySum(arr){
+    let maxi = -Infinity;
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+        if(sum > maxi){
+            maxi = sum
+        }
+        if(sum < 0){
+            sum = 0
+        }
+    }
+    return maxi
+    
+}
+console.log(maxSubarraySum([-2,1,-3,4,-1,2,1,-5,4]))
+
 
 
